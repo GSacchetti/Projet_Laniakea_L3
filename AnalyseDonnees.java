6919f168dc95a70ee8efb157e739c4de192c80f3
GSacchetti <48059787+GSacchetti@users.noreply.github.com>
2019-05-09 16:27:36 +0200

    Update AnalyseDonnees.java

--- AnalyseDonnees.java
+++ AnalyseDonnees.java
@@ -2,12 +2,12 @@ package Donnees;
 
 public class AnalyseDonnees {
 	
-	public static Vect3 Calcul_Coordonnées (Amas Amas) {
+	public static Vect3 Calcul_Coordonnées (Amas amas) {
 		Vect3 VectPos = new Vect3(0, 0, 0);
 		
-		int glon = Amas.glon;
-		int glat = Amas.glat;
-		int dist = Amas.dist;
+		int glon = amas.glon;
+		int glat = amas.glat;
+		int dist = amas.dist;
 		
 		VectPos.setX(dist*(Math.cos((double) glat))*(Math.sin((double) glon)));
 		VectPos.setY(dist*(Math.cos((double) glat))*(Math.cos((double) glon)));
