f27ad46f95abaa99d3af66da26ac8b719d69ab37
bausuw <50453267+bausuw@users.noreply.github.com>
2019-05-22 18:36:01 +0200

    Update AnalyseDonnees2.java

--- AnalyseDonnees2.java
+++ AnalyseDonnees2.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class AnalyseDonnees {
 	final static double G = 6.67408 * Math.pow(10.0, -11.0);
 	final static double MPC = 3.086 * Math.pow(10.0, 22.0);
-	final static double PC = 3.086 * Math.pow(10.0, 16.0);
+	//final static double PC = 3.086 * Math.pow(10.0, 16.0);
 	final static double TMsun = 1.9884 * Math.pow(10.0, 42.0);
 	final static int NB = 11508;
 
@@ -21,9 +21,9 @@ public class AnalyseDonnees {
 		vectPos.setZ(dist * (Math.sin(Math.toRadians(glat))));
 		
 		// vitesse propre initiale
-		/*amas.getVit().setX((vectPos.getX()/dist)*(amas.getVgsr()-75*(dist/PC))*1000);
-		amas.getVit().setY((vectPos.getY()/dist)*(amas.getVgsr()-75*(dist/PC))*1000);
-		amas.getVit().setZ((vectPos.getZ()/dist)*(amas.getVgsr()-75*(dist/PC))*1000);*/
+		/*amas.getVit().setX((vectPos.getX()/dist)*(amas.getVgsr()-75*(dist/MPC))*1000);
+		amas.getVit().setY((vectPos.getY()/dist)*(amas.getVgsr()-75*(dist/MPC))*1000);
+		amas.getVit().setZ((vectPos.getZ()/dist)*(amas.getVgsr()-75*(dist/MPC))*1000);*/
 		//--------
 		
 		return vectPos;
