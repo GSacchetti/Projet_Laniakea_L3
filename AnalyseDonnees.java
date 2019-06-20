40e75bfe435c8b10120c4d83d79f1770f5265186
Gros-G <50714419+Gros-G@users.noreply.github.com>
2019-06-20 16:06:08 +0200

    Add files via upload
    Modifications pour permettre de connaitre l'avancement en temps reel.

--- AnalyseDonnees.java
+++ AnalyseDonnees.java
@@ -1,15 +1,22 @@
 package Donnees;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.IOException;
+
+import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
 
 public class AnalyseDonnees {
 	final static double G = 6.67408 * Math.pow(10.0, -11.0);
 	final static int NB = 11508;
 	final static double MPC = 3.0856775814672 * Math.pow(10.0, 22.0);
-	static double avancement = 0;
 	static int H0 = 75;
 	static double VAR = H0 / MPC;
-
+	public static double avancement = 0;
+	
+	private static Amas[][] res;
+	
 	/**
 	 * Calcule la distance entre 2 amas
 	 * 
@@ -85,7 +92,7 @@ public class AnalyseDonnees {
 
 		return res;
 	}
-	
+
 	/**
 	 * Calcule la vitesse de l amas avec l expansion
 	 * @param i
@@ -95,14 +102,14 @@ public class AnalyseDonnees {
 	 */
 	public static Vect3 vitesseExp(int i, int j, Amas[][] t) {
 		double expX, expY, expZ, dist, x, y, z, a;
-		
+
 		x = t[i][j].getPos().getX() * t[i][j].getPos().getX();
 		y = t[i][j].getPos().getY() * t[i][j].getPos().getY();
 		z = t[i][j].getPos().getZ() * t[i][j].getPos().getZ();
 
 		dist = Math.sqrt(x + y + z);
 		a = 1/dist;
-		
+
 		expX = (t[i][j].getPos().getX() * a) * Math.abs((VAR * t[i][j].getPos().getX()));
 		expY = (t[i][j].getPos().getY() * a) * Math.abs((VAR * t[i][j].getPos().getY()));
 		expZ = (t[i][j].getPos().getZ() * a) * Math.abs((VAR * t[i][j].getPos().getZ()));
@@ -160,50 +167,81 @@ public class AnalyseDonnees {
 	 * @return Tableau avec toutes les positions de chaque amas
 	 * @throws IOException
 	 */
-	public static Amas[][] produitFinal(double dt, int frames, int opt) throws IOException {
-		double time = dt * 3.1536 * Math.pow(10, 13);// transformation du temps en seconde
+	public static Amas[][] produitFinal(double dt,final int frames,final int opt) throws IOException {
+		final double time = dt * 3.1536 * Math.pow(10, 13);// transformation du temps en seconde
 
-		Amas[][] res = new Amas[frames][NB];// initialisation du tableau
+		res = new Amas[frames][NB];// initialisation du tableau
 		LectureDonnees.donnees(res, time);
-		
-		if (opt == 0) {
-			for (int f = 1; f < frames - 1; f++) {
-				for (int j = 0; j < NB; j++) {
-					// pos2 est la position a t+1 qui est calcule l'instant t
-					res[f][j].setPos(Vect3.copie(res[f][j].getPos2()));// a chaque debut de boucle on remet la position2
-																		// a la position1
-					res[f][j].setVit(calculVit(f, j, res, time));// calcule de la vitesse
-					res[f][j].setPos2(calculPos(f, j, res, time));// calcule de la position pour la prochaine frame dans
-																	// pos2
-					res[f + 1][j] = Amas.copie(res[f][j]);// copie de l'amas a la frame+1
+
+
+
+		SwingWorker sw = new SwingWorker(){
+			protected Object doInBackground() throws Exception {
+
+
+				if (opt == 0) {
+					for (int f = 1; f < frames - 1; f++) {
+						for (int j = 0; j < NB; j++) {
+							// pos2 est la position a t+1 qui est calcule l'instant t
+							res[f][j].setPos(Vect3.copie(res[f][j].getPos2()));// a chaque debut de boucle on remet la position2
+							// a la position1
+							res[f][j].setVit(calculVit(f, j, res, time));// calcule de la vitesse
+							res[f][j].setPos2(calculPos(f, j, res, time));// calcule de la position pour la prochaine frame dans
+							// pos2
+							res[f + 1][j] = Amas.copie(res[f][j]);// copie de l'amas a la frame+1
+						}
+						avancement = (f+1)*100/(frames-1);
+						setProgress((int) avancement);
+						System.out.println(avancement + " %");
+					}
+
+					for (int j = 0; j < NB; j++) {
+						res[frames - 1][j].setPos(Vect3.copie(res[frames - 1][j].getPos2()));
+					}
+				} else {
+					for (int f = 1; f < frames - 1; f++) {
+						for (int j = 0; j < NB; j++) {
+							// pos2 est la position a t+1 qui est calcule l'instant t
+							res[f][j].setPos(Vect3.copie(res[f][j].getPos2()));// a chaque debut de boucle on remet la position2
+							// a la position1
+							res[f][j].getVit().addV(vitesseExp(f,j,res));// ajout de la vitesse d expansion
+							res[f][j].setVit(calculVit(f, j, res, time));// calcule de la vitesse
+							res[f][j].setPos2(calculPos(f, j, res, time));// calcule de la position pour la prochaine frame dans
+							// pos2
+							res[f + 1][j] = Amas.copie(res[f][j]);// copie de l'amas a la frame+1
+						}
+						avancement = (f+1)*100/(frames-1);
+						setProgress((int) avancement);
+						System.out.println(avancement + " %");
+					}
+
+					for (int j = 0; j < NB; j++) {
+						res[frames - 1][j].setPos(Vect3.copie(res[frames - 1][j].getPos2()));
+					}
 				}
-				avancement = f * 100 / (frames - 1);
-				System.out.println(avancement + " %");
+
+				return null;
 			}
 
-			for (int j = 0; j < NB; j++) {
-				res[frames - 1][j].setPos(Vect3.copie(res[frames - 1][j].getPos2()));
-			}
-		} else {
-			for (int f = 1; f < frames - 1; f++) {
-				for (int j = 0; j < NB; j++) {
-					// pos2 est la position a t+1 qui est calcule l'instant t
-					res[f][j].setPos(Vect3.copie(res[f][j].getPos2()));// a chaque debut de boucle on remet la position2
-																		// a la position1
-					res[f][j].getVit().addV(vitesseExp(f,j,res));// ajout de la vitesse d expansion
-					res[f][j].setVit(calculVit(f, j, res, time));// calcule de la vitesse
-					res[f][j].setPos2(calculPos(f, j, res, time));// calcule de la position pour la prochaine frame dans
-																	// pos2
-					res[f + 1][j] = Amas.copie(res[f][j]);// copie de l'amas a la frame+1
-				}
-				avancement = f * 100 / (frames - 1);
-				System.out.println(avancement + " %");
-			}
+			public void done(){ 
+				Main.Main.calcul_termine = true;
+				Main.Main.lancer_simulation();
+				Main.Main.request_frame.closeDialog();
+				Main.Main.request_frame.openSimulationNavigator();
+			}         
 
-			for (int j = 0; j < NB; j++) {
-				res[frames - 1][j].setPos(Vect3.copie(res[frames - 1][j].getPos2()));
-			}
-		}
+		};
+
+		sw.addPropertyChangeListener(new PropertyChangeListener(){
+			public void propertyChange(PropertyChangeEvent event) {
+				if("progress".equals(event.getPropertyName())){
+					if(SwingUtilities.isEventDispatchThread())
+						Main.Main.request_frame.dialog.updateComponents();
+				}            
+			}         
+		});
+
+		sw.execute();
 
 		return res;
 	}
